3940b8773b001bcd7a115d224ddaecdc247400f3
hakuturu583 <ms.kataoka@gmail.com>
2024-06-18 01:48:24 +0900

    add ACSSR
    Signed-off-by: hakuturu583 <ms.kataoka@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,55 @@
 #include <Arduino.h>
+#include <ctime>
+#include <M5_ACSSR.h>
 #include <M5_Ethernet.h>
 #include <M5Core2.h>
 #include <SPI.h>
-#include <ctime>
 
 #define CS 26
 
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(192, 168, 0, 210);
-IPAddress gateway(192, 168, 0, 1);
-IPAddress subnet(255, 255, 255, 0);
 
 EthernetServer server(8080);
+M5_ACSSR SSR;
+
+void scan_addr() {
+  int device_count = 0;
+  for (uint8_t addr = 1; addr < 127; addr++) {
+    Wire1.beginTransmission(addr);
+    if (Wire1.endTransmission() == 0) {
+      M5.Lcd.printf ("Device I2C Addr is: 0x%02X\r\n", addr);
+      ++device_count;
+    }
+  }
+  if(device_count == 0) {
+    M5.Lcd.println("IC2 addres could not found.");
+    while (true) {
+      delay(100);
+    }
+  }
+}
 
 void setup() {
-  M5.begin(true, false, false);
+  M5.begin(true, false, true);
   M5.Lcd.println("Battery Relay");
   M5.Lcd.println(" ");
   Ethernet.init(CS);
-  // Ethernet.begin(mac, ip, gateway, gateway, subnet);
   Ethernet.begin(mac, ip);
   delay(500);
-  M5.Lcd.print(Ethernet.localIP());
+  M5.Lcd.println(Ethernet.localIP());
+
+  scan_addr();
+  if(SSR.begin(&Wire1, 32, 33, 0x50)) {
+    M5.Lcd.println("Solid state relay found.");
+  }
+  else {
+    M5.Lcd.println("Solid state relay did not find.");
+    while(true) {
+      delay(100);
+    }
+  }
+  SSR.setLEDColor(0xff0000);
 
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
     Serial.println("Ethernet was not found, please check hardware connection.");
